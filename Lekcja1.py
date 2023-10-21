6076faace7eebfa0fa347457e19fb6ceb0fddb26
Prottos <patryk.kwas97@gmail.com>
2023-10-21 11:21:18 +0200

    Kosmetyka, dodano drugi plik

--- Lekcja1.py
+++ Lekcja1.py
@@ -1,22 +1,22 @@
-#to jest komentarz - tego nie wykona program
+# to jest komentarz - tego nie wykona program
 
-#print("Dzień dobry") #funkcja print() wyświetla elemnt zawarty w (), jeśli chemy wyświetlić tekst dajemy w nawiasie ""
-#print('Dzień dobry') #lub ''
-a = 5       #zmienna liczbowa
-b = 6.5     #ułamki liczb - dajemy . a nie ,           typ danych - integer (int)
-c = "kotek" #zmienna tekstowa musi być w "" lub ''     typ danych - string (str)
+# print("Dzień dobry") # funkcja print() wyświetla elemnt zawarty w (), jeśli chemy wyświetlić tekst dajemy w nawiasie ""
+# print('Dzień dobry') # lub ''
+a = 5       # zmienna liczbowa                               typ danych - integer (int)
+b = 6.5     # liczba zmiennoprzecinkowa - dajemy . a nie ,   typ danych - float (float)
+c = "kotek" # zmienna tekstowa musi być w "" lub ''          typ danych - string (str)
 
-#print("a")  #aby użyć zmiennej nie dajemy "" lub '', ponieważ odczyta to jako tekst
+# print("a")  # aby użyć zmiennej nie dajemy "" lub '', ponieważ odczyta to jako tekst
 
-#print(a)    #jeśli nie ma zmiennej wywali błąd - name '' is not defined
-#print(a+c) #nie można dodać liczby do napisu
-#print(a*c)  #można mnożyć liczbę i napis, ale tylko liczbę całkowitą
-#print(c*c)  #nie można mnożyć napisu z napisem
+# print(a)      # jeśli nie ma zmiennej wywali błąd - name '' is not defined
+# print(a+c)    # nie można dodać liczby do napisu
+# print(a*c)    # można mnożyć liczbę i napis, ale tylko liczbę całkowitą
+# print(c*c)    # nie można mnożyć napisu z napisem
 d = a + b + 4
 print(d)
-print(a * (c + ' ')) #można do siebie dodać napisy
+print(a * (c + ' ')) # można do siebie dodać napisy
 
-print('\n') #tworzy nową linię
+print('\n')          # tworzy nową linię
 
 liczba1 = 3
 liczba2 = '3'
@@ -24,5 +24,6 @@ liczba2 = '3'
 print(3 * liczba1)
 print(2 * liczba2)
 
-print(type(liczba1))    #sprawdza typ danych (class) np int, str
+print(type(liczba1))    # sprawdza i wyświetla typ danych (class) np int, str, float
 print(type(liczba2))
+
