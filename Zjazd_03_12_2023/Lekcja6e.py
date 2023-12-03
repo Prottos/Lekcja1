e0b471398d24b5abe1f78c6265a95a27daa30665
Prottos <patryk.kwas97@gmail.com>
2023-12-03 14:28:36 +0100

    obiektowka poczatek

---
+++ Zjazd_03_12_2023/Lekcja6e.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self):
+        self.color = "red"
+        self.fuel = 10
+        self.condition = 5
+        self.eco_mode = False
+        self.fuel_consumption = 14
+
+my_car = Car()
+neighbor_car = Car()
