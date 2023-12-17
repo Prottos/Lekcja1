bd77f8487f0e3cf8cd54f67ac349e6eb64f24013
Prottos <patryk.kwas97@gmail.com>
2023-12-17 09:59:20 +0100

    opisy

--- Zjazd_17_12_2023/Lekcja8a.py
+++ Zjazd_17_12_2023/Lekcja8a.py
@@ -9,11 +9,21 @@ class CityAnalizer:
         print(self.city_avg)                                                                # można dać w liście wiele różnych danych (jak w przykładzie wyżej)
 
     def plot (self):
-        self.city_avg.plot(kind = "bar", figsize = (10, 6))                                 # tworzenie wykresu słupkowego (kind = "bar"), można dodać więcej argumentów, np kolor, rozmiar
+        ax = self.city_avg.plot(kind = "bar", figsize = (10, 6))                                 # tworzenie wykresu słupkowego (kind = "bar"), można dodać więcej argumentów, np kolor, rozmiar, dodaję odwołanie do wykresu celem dalszego wprowadzenia opisu
         plt.title("Średni wzrost w miastach")                                               # nadawanie tytułu wykresu
         plt.xlabel("Miasto")                                                                # opis kolumn
         plt.xticks(rotation = 0)                                                            # obrót opisu kolumn
         plt.ylabel("Średni wzrost")                                                         # opis wartości
+        # for i, value in enumerate(self.city_avg):
+        #     plt.annotate(f"{value:.2f}", (i, value), textcoords="offset points", xytext=(0, 5), ha="center")    # dodawanie opisu maksymalnej wartości do kolumn
+        for i in ax.patches:                                                                # inna opcja dodwania opisów już na podstawie istniejącego wykresu
+            height = i.get_height()                                                         # pobieramy z wykresu najwyższe wartości kolumn
+            plt.annotate(f"{height:.2f}", 
+                         (i.get_x() + i.get_width() / 2, height), 
+                         textcoords = "offset points", 
+                         xytext = (1, -35), 
+                         ha = "center",
+                         rotation = 90)      # teraz możemy dodać i opisać więcej danych w jednym wykresie
         plt.show()                                                                          # wyświetlanie wykresu
 
 city = CityAnalizer()
